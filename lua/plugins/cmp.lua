942ff5be7d014a9bd7100e0e17235e0aed5579d5
Farid <feridagazade157@gmail.com>
2026-03-28 22:32:37 +0400

    Added more application.properties names for cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -29,51 +29,8 @@ return {
             local luasnip = require("luasnip")
 
             -- Custom Spring boot properties completion soruce
-            local spring_source = {}
-
-            spring_source.new = function ()
-                return setmetatable({}, {__index = spring_source })
-            end
-
-            spring_source.is_available = function()
-                return vim.bo.filetype == "jproperties"
-            end
-
-            spring_source.get_trigger_characters = function()
-                return { "." }
-            end
-
-            spring_source.complete = function(self, request, callback)
-                local items = {
-                    { label = "spring.datasource.url", documentation = "JDBC URL of the database" },
-                    { label = "spring.datasource.username", documentation = "Login username of the database" },
-                    { label = "spring.datasource.password", documentation = "Login password of the database" },
-                    { label = "spring.datasource.driver-class-name", documentation = "Fully qualified name of the JDBC driver" },
-                    { label = "spring.jpa.hibernate.ddl-auto", documentation = "DDL mode: none, validate, update, create, create-drop" },
-                    { label = "spring.jpa.show-sql", documentation = "Whether to enable logging of SQL statements" },
-                    { label = "spring.jpa.properties.hibernate.dialect", documentation = "Hibernate dialect" },
-                    { label = "spring.jpa.properties.hibernate.format_sql", documentation = "Whether to format SQL output" },
-                    { label = "server.port", documentation = "Server HTTP port" },
-                    { label = "server.servlet.context-path", documentation = "Context path of the application" },
-                    { label = "spring.application.name", documentation = "Application name" },
-                    { label = "spring.profiles.active", documentation = "Active profiles" },
-                    { label = "spring.security.user.name", documentation = "Default user name" },
-                    { label = "spring.security.user.password", documentation = "Default user password" },
-                    { label = "logging.level.root", documentation = "Root log level: TRACE, DEBUG, INFO, WARN, ERROR" },
-                    { label = "logging.level.org.springframework", documentation = "Spring framework log level" },
-                    { label = "logging.file.name", documentation = "Log file name" },
-                    { label = "spring.cache.type", documentation = "Cache type to use" },
-                    { label = "spring.data.redis.host", documentation = "Redis server host" },
-                    { label = "spring.data.redis.port", documentation = "Redis server port" },
-                    { label = "spring.mail.host", documentation = "SMTP server host" },
-                    { label = "spring.mail.port", documentation = "SMTP server port" },
-                    { label = "spring.mail.username", documentation = "Login user of the SMTP server" },
-                    { label = "spring.mail.password", documentation = "Login password of the SMTP server" },
-                }
-                callback({ items = items, isIncomplete = false })
-            end
-
-            require("cmp").register_source("spring_properties", spring_source)
+            local spring_source = require("custom.cmp_spring")
+            require("cmp").register_source("spring_properties", spring_source.new())
 
             -- Lazily load the vscode like snippets
             require("luasnip.loaders.from_vscode").lazy_load()
